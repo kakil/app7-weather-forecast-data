8c8445eb9d47c14adcc88de234df2169fa2e78b3
Kitwana Akil <kakil@me.com>
2024-05-20 14:59:36 -0400

    Day33: Completed implementing the application.  Added city check.  Added conversion to Fahrenheit.  Formatted images to show 8 images per row(day).

--- backend.py
+++ backend.py
@@ -10,7 +10,8 @@ api_key = os.getenv("OPEN_WEATHER_API_KEY")
 # http://api.openweathermap.org/data/2.5/forecast?q=jacksonville&appid=e118605d36f22d7ae480a12ccff0fab0
 # http://api.openweathermap.org/data/2.5/forecast?q=orange%20park&appid=e118605d36f22d7ae480a12ccff0fab0
 
-def get_data(city, forecast_days, kind):
+
+def get_data(city, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
@@ -18,11 +19,11 @@ def get_data(city, forecast_days, kind):
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
 
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
+
+
+def convert_to_fahrenheit(temperature):
+    return ((temperature / 10) * (9/5)) + 32
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-from backend import get_data
+from backend import get_data, convert_to_fahrenheit
 
-st.header("Weather Forecast for the Upcoming Days")
+
+# Add title, text input, slider, selectbox, and subheader
+st.title("Weather Forecast for the Upcoming Days")
 
 city = st.text_input(label="Place:")
 
@@ -15,12 +17,31 @@ days = st.slider(label="Forecast Days",
 option = st.selectbox(label="Select data to view",
                          options=["Temperature", "Sky"])
 
-st.subheader(f"{option} for the next {days} in {city}")
+if city:
+    try:
+        filtered_data = get_data(city,days)
 
-data = get_data(city, days, option)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            new_temperatures = [convert_to_fahrenheit(temp) for temp in temperatures]
+
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=new_temperatures, labels={"x": "Date", "y": "Temperature (F)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=85)
+
+        st.subheader(f"{option} for the next {days} day(s) in {city}")
+    except (NameError, KeyError):
+        st.error("City NOT FOUND!")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 
-d, t = get_data(days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
-st.plotly_chart(figure)
